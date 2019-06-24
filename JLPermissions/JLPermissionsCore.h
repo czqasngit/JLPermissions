ade37a5cbeed49c9dc33ff4ce12a139a49bef81f
坏人 <czqasn_6@163.com>
2019-06-24 13:26:54 +0800

    @property(nonatomic, getter=isExtraAlertEnabled,) BOOL extraAlertEnabled; -> @property(nonatomic,assign) BOOL extraAlertEnabled;

--- JLPermissions/JLPermissionsCore.h
+++ JLPermissions/JLPermissionsCore.h
@@ -37,7 +37,7 @@ typedef void (^NotificationAuthorizationHandler)(NSString *__nullable deviceID,
  * A Boolean property that indicates whether the extra alert view will be shown
  * before the library actually requests permissions to the system.
  */
-@property(nonatomic, getter=isExtraAlertEnabled) BOOL extraAlertEnabled;
+@property(nonatomic,assign) BOOL extraAlertEnabled;
 
 /**
  * @return whether or not user has granted access to the calendar
